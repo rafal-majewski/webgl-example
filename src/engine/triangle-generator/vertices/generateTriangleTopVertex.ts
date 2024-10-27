4357da9de27c927f2de3302880f05683e4106039
Rafał Majewski <goodheropl@gmail.com>
2024-10-27 08:19:08 +0100

    feature: Add animating

--- src/engine/triangle-generator/vertices/generateTriangleTopVertex.ts
+++ src/engine/triangle-generator/vertices/generateTriangleTopVertex.ts
@@ -5,7 +5,7 @@ import type {Vertex} from "../../Vertex.js";
 export function generateTriangleTopVertex(): Vertex {
 	const position: Coordinates = {
 		x: 0,
-		y: 0.5,
+		y: 0.5 + Math.sin(Date.now() / 1000) / 2,
 	};
 
 	const color: RgbColor = {
