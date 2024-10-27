db8661d1f5ce59b0cbe45f3c6e0681aeebfd3f53
Rafał Majewski <goodheropl@gmail.com>
2024-10-27 15:12:00 +0100

    temp

--- src/engine/camera/Camera.ts
+++ src/engine/camera/Camera.ts
@@ -1,5 +1,7 @@
 import type {Coordinates} from "../Coordinates.js";
+import type {Orientation} from "../orientation/Orientation.js";
 
 export type Camera = Readonly<{
 	position: Coordinates;
+	orientation: Orientation;
 }>;
