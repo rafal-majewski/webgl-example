6274d0cc22b4df9182c5ea0af9622dbe77405035
Rafał Majewski <goodheropl@gmail.com>
2024-10-29 10:11:13 +0100

    chore: Configure releasing to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 /** @type {import("vite").UserConfig} */
 const viteConfig = {
 	root: "./src",
+	build: {
+		outDir: "../build",
+		emptyOutDir: true,
+	},
+	base: process.env["GITHUB_ACTIONS"] === "true" ? "/webgl-example/" : "/",
 };
 
 export default viteConfig;
